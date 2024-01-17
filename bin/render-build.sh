e70dd5fbb280758d5f7684c63b4128d71d1e7e1e
wonderfullworld5 <bbb893576@gmail.com>
2024-01-17 15:25:22 +0900

    変更したコード

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,9 +1,8 @@
-#!/usr/bin/env bash
+!/usr/bin/env bash
 # exit on error
 set -o errexit
 
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
-
+bundle exec rake db:migrate
